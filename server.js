0b1be5cae7cedf82baee3e0dca6693207fef3b4c
AdamM <ajmrozinski@gmail.com>
2019-11-07 17:03:33 -0500

    rebuilt react client folder and added scrips to package.json for running both files on start

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require("express");
-const path = require ("path");
-const port = process.env.port || 3001;
+const path = require("path");
+const PORT = process.env.port || 3001;
 const app = express();
 
-
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"))
-};
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-
-
-
-app.listen(PORT, ()=>{
-    console.log(`Server is listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
