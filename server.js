aad45c89272cd3a9c53472b2ad8218fd7440942b
AdamM <ajmrozinski@gmail.com>
2019-11-07 16:18:11 -0500

    replaced budget buddy with a create react app client

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require ("path");
+const port = process.env.port || 3001;
+const app = express();
+
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+};
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`)
+})
