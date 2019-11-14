dc99f70aaa9002efb91e6efbc83a0db801288490
AdamM <ajmrozinski@gmail.com>
2019-11-14 14:13:56 -0500

    firebase auth config file and functions set up

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../../controllers/usersController");
+const authChecker = require("../../controllers/authMiddleware")
 
 router.route("/")
-.get(usersController.findOneUser)
+.get(authChecker,usersController.findOneUser)
 .post(usersController.createUser)
 .delete(usersController.deleteUser)
 
